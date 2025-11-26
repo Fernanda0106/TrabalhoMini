136d3e4af7a9d2a16028c95005b9e7455ca0c234
isah2882 <conceicaodesouzaisadora@gmail.com>
2025-11-26 09:39:28 -0300

    FOII PORAAAAA

--- TrabalhoMini/src/App.jsx
+++ TrabalhoMini/src/App.jsx
@@ -1,13 +1,40 @@
 import './App.css'
+//Pages
 import Home from './pages'
+import Cart from './pages/Cart'
 
-function App() {
+//Components
+import Header from './components/Header'
+import Footer from './components/Footer'
+import ProductCard from './components/ProductCard'
+import HeroBanner from './components/HeroBanner'
+import LoadingSpinner from './components/LoadingSpinner'
+import Modal from './components/Modal'
 
+//Admin
+import AdminDashboard from './admin/Dashboard'
+import ProductForm from './admin/ProductForm'
+import ProductList from './admin/ProductList'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+export default function App() {
   return (
-    <>
-      <Home />
-    </>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <HeroBanner/>
+        <main style={{padding:"20px", minHeight:"70vh"}}>
+          <Routes>
+            <Route path='/' element={<Home/>} />
+
+            <Route path='/cart' element={<Cart/>} />
+            <Route path='/admin/dashboard' element={<AdminDashboard/>} />
+            <Route path='/admin/products' element={<ProductList/>} />
+            <Route path='/admin/products/new' element={<ProductForm/>} />
+            <Route path='/admin/products/edit/:id' element={<ProductForm/>} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   )
 }
-
-export default App
--- TrabalhoMini/src/pages/Cart.jsx
+++ TrabalhoMini/src/pages/Cart.jsx
@@ -1,4 +1,4 @@
-import { useCart } from '../contexts/CartContext';
+
 import { Button } from 'primereact/button';
 import { Card } from 'primereact/card';
 import { Divider } from 'primereact/divider';

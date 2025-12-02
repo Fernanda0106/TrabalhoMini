4337bf43d10fe1f97fef6c360d6b2853244b3d51
isah2882 <conceicaodesouzaisadora@gmail.com>
2025-12-02 10:34:14 -0300

    sera?///

--- TrabalhoMini/src/App.jsx
+++ TrabalhoMini/src/App.jsx
@@ -11,33 +11,36 @@ import ProductCard from './components/ProductCard'
 import HeroBanner from './components/HeroBanner'
 import LoadingSpinner from './components/LoadingSpinner'
 import Modal from './components/Modal'
-import { CartProvider } from './components/CartContex'
 
 //Admin
 import AdminDashboard from './admin/Dashboard'
 import ProductForm from './admin/ProductForm'
 import ProductList from './admin/ProductList'
+import CartContext  from './Context/CartContex'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <AuthProvider>
       <CartProvider>
-        <div className="App">
-   
-          <main style={{padding:"20px", minHeight:"70vh"}}>
-            <Routes>
-              <Route path='/' element={<Home/>} />
-              <Route path='/product/:id' element={<ProductDetails/>} />
-              <Route path='/Cart' element={<Cart/>} />
-              <Route path='/admin/dashboard' element={<AdminDashboard/>} />
-              <Route path= '/products' element={<ProductList/>} />
-              <Route path='/admin/products/new' element={<ProductForm/>} />
-              <Route path='/admin/products/edit/:id' element={<ProductForm/>} />
-            </Routes>
-          </main>
-        </div>
+        <BrowserRouter>
+          <div className="App">
+            <Header />
+            <HeroBanner/>
+            <main style={{padding:"20px", minHeight:"70vh"}}>
+              <Routes>
+                <Route path='/' element={<Home/>} />
+                <Route path='/cart' element={<Cart/>} />
+                <Route path='/admin/dashboard' element={<AdminDashboard/>} />
+                <Route path='/admin/products' element={<ProductList/>} />
+                <Route path='/admin/products/new' element={<ProductForm/>} />
+                <Route path='/admin/products/edit/:id' element={<ProductForm/>} />
+              </Routes>
+            </main>
+            <Footer/>
+          </div>
+        </BrowserRouter>
       </CartProvider>
-    </BrowserRouter>
+    </AuthProvider>
   )
 }
